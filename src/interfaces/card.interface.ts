9e66738a3ae27e18659af3b45fb9877972b85349
Francisco Javier Camacho Guerra <francisco.camacho@mavendiagnostics.com>
2023-10-02 00:46:21 -0600

    first api call

--- src/interfaces/card.interface.ts
+++ src/interfaces/card.interface.ts
@@ -1,4 +1,35 @@
 export interface PropsCard {
     expandedCard?: boolean;
-    width?: string;
+    width?: any;
+}
+
+export interface CardSubCategoryProps {
+    subCategories?: Array<SubCategories>
+}
+
+export interface SubCategories {
+    subCategoryId?: any;
+    categoryId?: any;
+    name?: any;
+    description?:  any;
+    displayStatus?: any;
+    BtinHeaderAndFooter?: any;
+    ftinBannerIcon?: any;
+    ftinTitle?: any;
+    icon?:  any;
+    useExternalUrl?: any;
+    categoryLandPageDesc?:  any;
+    categoryLandPageHead?:  any;
+    subCategoryLandPageDesc?:  any;
+    isActive?: any;
+    static?: any;
+    seotitle?: any;
+    seoprettyUrl?:  any;
+    seodescMetadata?:  any;
+    createdDate?:  any;
+    updatedDate?:  any;
+    deleteFlag?: any;
+    rowVersion?:  any;
+    subCategoryThumbNails?: Array<any>;
+    ftinHeaderAndFooter?: any;
 }
